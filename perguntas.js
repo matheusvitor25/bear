e84ace90dd51ce5b6c4cf2e5cc4af1a81e101e91
matheusvitor25 <137187196+matheusvitor25@users.noreply.github.com>
2024-10-17 09:53:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual e a capital do brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Geografia',
+    'quantas letras tem o alfabeto?',
+    '26 letras'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma camera?',
+    'Maquina para fazer foografias e filmagens'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz carro em Inglês?',
+    'Carro em ingles é car '
+)
